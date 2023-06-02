b6cab3f1726164a5c5533b53ed09bb2418a56085
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-03 03:51:09 +0600

    added json web token and securre admin and user pannel

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -25,55 +43,111 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const usersCollections=client.db('bistroDB').collection('users')
-    const menuCollections=client.db('bistroDB').collection('menu')
-    const reviewsCollections=client.db('bistroDB').collection('reviews')
-    const cartsCollections=client.db('bistroDB').collection('carts')
+    const usersCollections = client.db('bistroDB').collection('users')
+    const menuCollections = client.db('bistroDB').collection('menu')
+    const reviewsCollections = client.db('bistroDB').collection('reviews')
+    const cartsCollections = client.db('bistroDB').collection('carts')
+
+    // JWT
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      })
+      res.send({ token })
+    })
+
+      // Warning: use verifyJWT before using verifyAdmin
+      const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email }
+        const user = await usersCollections.findOne(query);
+        if (user?.role !== 'admin') {
+          return res.status(403).send({ error: true, message: 'forbidden message' });
+        }
+        next();
+      }
 
     //  users
 
-    app.get('/users', async (req,res)=>{
-      const result=await usersCollections.find().toArray()
+    app.get('/users',verifyJWT,verifyAdmin, async (req, res) => {
+      const result = await usersCollections.find().toArray()
       res.send(result)
     })
 
-    app.post('/users',async(req,res)=>{
-      const user=req.body;
-      const query={email: user.email}
-      const existingUser= await usersCollections.findOne(query)
-      if(existingUser){
-        return res.send({message: 'user already exist'})
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollections.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user already exist' })
       }
       const result = await usersCollections.insertOne(user)
       res.send(result)
     })
 
-    //  menu 
-    app.get('/menu', async(req,res)=>{
-        const result= await menuCollections.find().toArray()
-        res.send(result)
+     // security layer: verifyJWT
+    // email same
+    // check admin
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+
+      const query = { email: email }
+      const user = await usersCollections.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
     })
-  //  review
-    app.get('/review', async(req,res)=>{
-        const result= await reviewsCollections.find().toArray()
-        res.send(result)
+
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollections.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+    //  menu 
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollections.find().toArray()
+      res.send(result)
+    })
+    //  review
+    app.get('/review', async (req, res) => {
+      const result = await reviewsCollections.find().toArray()
+      res.send(result)
     })
 
     // carts collections
 
-    app.get('/carts', async(req,res)=>{
+    app.get('/carts', verifyJWT, async (req, res) => {
       const email = req.query.email;
-      if(!email){
+      if (!email) {
         res.send([])
       }
-      else{
-        const query={ email: email};
+
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'porviden access' })
+      }
+
+      else {
+        const query = { email: email };
         const result = await cartsCollections.find(query).toArray()
         res.send(result)
       }
     })
 
-    app.post('/carts', async(req,res)=>{
+    app.post('/carts', async (req, res) => {
       const item = req.body;
       console.log(item)
       const result = await cartsCollections.insertOne(item)
@@ -81,9 +155,9 @@ async function run() {
     })
 
 
-    app.delete('/carts/:id', async(req,res)=>{
+    app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id;
-      const query={_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await cartsCollections.deleteOne(query)
       res.send(result)
     })
@@ -100,10 +174,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req,res)=>{
-    res.send('bistro boss is sitting....')
+app.get('/', (req, res) => {
+  res.send('bistro boss is sitting....')
 })
 
-app.listen(port,()=>{
-    console.log(`bistro boss is sitting on port ${port}`)
+app.listen(port, () => {
+  console.log(`bistro boss is sitting on port ${port}`)
 })
